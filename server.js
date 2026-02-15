801fd7a14c72f0208a00101a38d419082d98bde2
Reda Darwiche <redadarwiche2020@gmail.com>
2026-02-15 12:12:24 -0800

    Refactor server.js for improved structure and CORS

--- server.js
+++ server.js
@@ -4,25 +4,28 @@ const socketIo = require('socket.io');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+
+// Enable CORS for all routes
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
 
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+        origin: '*',
+        methods: ['GET', 'POST'],
+        credentials: true
+    },
+    transports: ['websocket', 'polling']
 });
 
 const PORT = process.env.PORT || 3000;
 
 // Store all connected players
 const players = {};
-const bots = {};
-
-// Game constants
-const CANVAS_WIDTH = 8000;
-const CANVAS_HEIGHT = 8000;
 
 io.on('connection', (socket) => {
     console.log('New player connected:', socket.id);
@@ -31,8 +34,8 @@ io.on('connection', (socket) => {
     socket.on('playerJoin', (data) => {
         players[socket.id] = {
             id: socket.id,
-            x: data.x || Math.random() * CANVAS_WIDTH,
-            y: data.y || Math.random() * CANVAS_HEIGHT,
+            x: data.x || 4000,
+            y: data.y || 4000,
             angle: data.angle || 0,
             name: data.name || 'Player',
             color: data.color || '#ff6b6b',
@@ -58,17 +61,13 @@ io.on('connection', (socket) => {
             players[socket.id].x = data.x;
             players[socket.id].y = data.y;
             players[socket.id].angle = data.angle;
-            players[socket.id].vx = data.vx;
-            players[socket.id].vy = data.vy;
 
             // Broadcast to all other players
             socket.broadcast.emit('playerMoved', {
                 id: socket.id,
                 x: data.x,
                 y: data.y,
-                angle: data.angle,
-                vx: data.vx,
-                vy: data.vy
+                angle: data.angle
             });
         }
     });
@@ -96,40 +95,10 @@ io.on('connection', (socket) => {
         }
     });
 
-    // When a player dashes
-    socket.on('playerDash', () => {
-        socket.broadcast.emit('playerDashed', socket.id);
-    });
-
-    // When a player uses ability
-    socket.on('playerAbility', (data) => {
-        socket.broadcast.emit('playerUsedAbility', {
-            id: socket.id,
-            ability: data.ability
-        });
-    });
-
-    // When a player attacks
-    socket.on('playerAttack', (targetId) => {
-        socket.broadcast.emit('playerAttacked', {
-            attackerId: socket.id,
-            targetId: targetId
-        });
-    });
-
     // When a player dies
     socket.on('playerDied', () => {
         socket.broadcast.emit('playerDeath', socket.id);
         delete players[socket.id];
-    });
-
-    // Chat message
-    socket.on('chatMessage', (data) => {
-        io.emit('chatMessage', {
-            id: socket.id,
-            name: players[socket.id]?.name || 'Unknown',
-            message: data.message
-        });
     });
 
     // When a player disconnects
